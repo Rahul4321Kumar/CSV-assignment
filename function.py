5e65124dc5ff2c5c476714eff16edaa1afea7c0d
Rahul4321Kumar <rkumar@deqode.com>
2021-12-04 20:33:53 +0530

    Changes done as per pr review

--- function.py
+++ function.py
@@ -1,34 +1,41 @@
 import csv
 
 
-def readcsv(filename1):
-    with open(filename1, 'r') as csvinput:
-        writecsv("output.csv",csvinput)
+def read_csv(filename):
+    try:
+        with open(filename)as csv_file:
+            print(csv_file.read())
+    except:
+        print(f'No file name like {filename} check it again')
+    
 
-
-def writecsv(filename2,csvinput):
-    with open(filename2, 'w') as csvoutput:
-        writer = csv.writer(csvoutput, lineterminator='\n')
-        reader = csv.reader(csvinput)
-
-        all = []
-        row = next(reader)
-        # print(reader[0])
-        row.append('ProductSaleaTax')
-        row.append('ProductFinalPrice')
-        all.append(row)
-        # print(all)
-
-        for row in reader:
-            row.append(20)
-            res = int(row[1])*0.20
-            price = int(row[1])+res
-            row.append(price)
-
+def write_csv(filename,filename1):
+    with open(filename, 'r') as csvinput:
+        with open(filename1, 'w') as csvoutput:
+            writer = csv.writer(csvoutput, lineterminator='\n')
+            reader = csv.reader(csvinput)
+            all=[]
+            print(reader)
+            row = next(reader)
+            row.append('ProductSaleaTax')
+            row.append('ProductFinalPrice')
             all.append(row)
+            logic_to_fill_newcolumn(reader,writer,all)
+    
+def logic_to_fill_newcolumn(reader,writer,all):
 
-        writer.writerows(all)
+    for row in reader:
+        row.append(20)
+        res = int(row[1])*0.20
+        price = int(row[1])+res
+        row.append(price)
+        all.append(row)
+    writer.writerows(all)
+    
+def csv_output( filename,filename1):
+    read_csv(filename)
+    write_csv(filename, filename1)
 
 if __name__ == "__main__":
-    readcsv("Product1.csv")
+    csv_output("Product1.csv","Output.csv")
         
