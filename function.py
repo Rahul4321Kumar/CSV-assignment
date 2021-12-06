b9897196cc66b150fe36a82b0105f356d691de28
Rahul4321Kumar <rkumar@deqode.com>
2021-12-06 15:56:30 +0530

    Changes done as per today review

--- function.py
+++ function.py
@@ -2,39 +2,39 @@ import csv
 
 
 def read_csv(filename):
-    try:
-        with open(filename)as csv_file:
-            print(csv_file.read())
-    except:
-        print(f'No file name like {filename} check it again')
+    with open(filename)as csv_file:
+        reader = csv.reader(csv_file)
+        lst=[]
+        for i in reader:
+            test_list = list(filter(None, i))
+            lst.append(test_list)
+    return lst
     
 
-def write_csv(filename,filename1):
-    with open(filename, 'r') as csvinput:
+def write_csv(filename1, val):
         with open(filename1, 'w') as csvoutput:
             writer = csv.writer(csvoutput, lineterminator='\n')
-            reader = csv.reader(csvinput)
-            all=[]
-            print(reader)
-            row = next(reader)
+            all = []
+            row = val[0]
             row.append('ProductSaleaTax')
             row.append('ProductFinalPrice')
             all.append(row)
-            logic_to_fill_newcolumn(reader,writer,all)
+            logic_to_fill_newcolumn(val,writer,all)
     
-def logic_to_fill_newcolumn(reader,writer,all):
-
-    for row in reader:
+def logic_to_fill_newcolumn(val,writer,all):
+    
+    for row in val[1:]:
         row.append(20)
         res = int(row[1])*0.20
         price = int(row[1])+res
         row.append(price)
+        print(row)
         all.append(row)
     writer.writerows(all)
     
 def csv_output( filename,filename1):
-    read_csv(filename)
-    write_csv(filename, filename1)
+    val=read_csv(filename)
+    write_csv(filename1, val)
 
 if __name__ == "__main__":
     csv_output("Input.csv","Output.csv")
