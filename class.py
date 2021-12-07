98d1a66e3e107d34633600fabf1c872f19aec8f8
Rahul4321Kumar <rkumar@deqode.com>
2021-12-07 13:11:18 +0530

    changes done as per today review

--- class.py
+++ class.py
@@ -5,13 +5,18 @@ class CsvOperation:
      def __init__(self,filename):
           self.filename = filename
 
-     def csv_output(self,filename1):
-         val=function.read_csv(self.filename)
-         function.write_csv(filename1,val)
+     def csv_output(self,filename1,filename2):
+          HEADER_VALIDATE=["Product_Name","Product-CostPrice","Country"]
+          val=function.read_csv(self.filename)
+          function.file_validation(filename2)
+          function.column_name_validate(val,HEADER_VALIDATE)
+          function.csv_datatype_validate(val)
+          function.write_csv(filename1,val)
+          
 
 if __name__ == '__main__':
      result = CsvOperation("Input.csv")
-     result.csv_output("Output.csv")
+     result.csv_output("Output.csv","Example.csv")
 
 
 
--- function.py
+++ function.py
@@ -1,12 +1,46 @@
 import csv
 
 
+def file_validation(filename2):
+    if not filename2.endswith(".csv"):
+        raise FileNotFoundError("filename2 is not a valid csv file")
+    else:
+        print("Its a valid csv file")
+
+def csv_datatype_validate(val):
+    for i in val[1:]:
+        if isinstance(i[0],str):
+            print("all data are in correct format")
+        else:
+            print("data in productname are not in correct format")
+
+        if isinstance(i[1],int):
+            print("all data are in correct format")
+        else:
+            print("data in costprice are not in correct format")
+            isinstance(int(i[1]),int)
+
+        if isinstance(i[2],str):
+            print("all data are in correct format")
+        else:
+            print("data in country are not in correct format")
+
+
+def column_name_validate(val,header):
+        print(val[0])
+        if val[0]==header:
+            print("All header matched")
+        else:
+            print("Issue in header do changes in input of HEADER_VALIDATE")
+        
+    
+        
 def read_csv(filename):
     with open(filename)as csv_file:
         reader = csv.reader(csv_file)
         lst=[]
-        for i in reader:
-            test_list = list(filter(None, i))
+        for read_data in reader:
+            test_list = list(filter(None, read_data))
             lst.append(test_list)
     return lst
     
@@ -22,20 +56,23 @@ def write_csv(filename1, val):
             logic_to_fill_newcolumn(val,writer,all)
     
 def logic_to_fill_newcolumn(val,writer,all):
-    
     for row in val[1:]:
         row.append(20)
         res = int(row[1])*0.20
         price = int(row[1])+res
         row.append(price)
-        print(row)
         all.append(row)
     writer.writerows(all)
     
-def csv_output( filename,filename1):
+def csv_output( filename,filename1,filename2):
+    HEADER_VALIDATE=["Product_Name","Product-CostPrice","Country"]
     val=read_csv(filename)
+    csv_datatype_validate(val)
+    column_name_validate(val,HEADER_VALIDATE)
     write_csv(filename1, val)
+    file_validation(filename2)
+    
 
 if __name__ == "__main__":
-    csv_output("Input.csv","Output.csv")
+    csv_output("Input.csv","Output.csv","Example.csv")
         
